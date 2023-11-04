b3f5fe4a0eb9e883b3da9373de9ab189fb8339de
Chris <chrisgalera1555@gmail.com>
2023-11-04 05:21:28 -0300

    Observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes'
 import { Hero } from './hero.model';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES)
+    return heroes;
   }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -18,7 +18,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes()
+    this.heroService.getHeroes().subscribe(heroes =>
+      this.heroes = heroes)
   }
 
   onSelect(hero: Hero): void {
